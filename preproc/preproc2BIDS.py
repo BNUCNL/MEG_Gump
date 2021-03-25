2c9efb53b49f2164e9da5114abde8798c51b9504
Xingyu Liu <liuxingyu987@gmail.com>
2021-03-25 23:58:26 +0800

    update

--- preproc/preproc2BIDS.py
+++ preproc/preproc2BIDS.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 import subprocess
 import glob
+import mne_bids
 
 # %%
 parent_dir = '/nfs/e5/studyforrest/forrest_movie_meg'
@@ -30,6 +31,9 @@ study_ass_files = ['dataset_description.json',
                    'participants.json',
                    'participants.tsv',
                    'README']
+mri_study_ass_files = ['dataset_description.json',
+                       'desc-aparcaseg_dseg.tsv',
+                       'desc-aseg_dseg.tsv']
 ses_ass_files = ['*_ses-movie_scans.tsv']
 meg_ass_files = ['*_ses-movie_coordsystem.json',
                  '*_channels.tsv',
@@ -37,7 +41,6 @@ meg_ass_files = ['*_ses-movie_coordsystem.json',
                  '*_meg.json']
 
 # %% copy meg associated files to preproced meg
-
 sublist = ['{0:0>2d}'.format(i) for i in np.arange(1, 12)]
 for sub in sublist:
     sub_raw_ses_dir = os.path.join(raw_data, 'sub-{0}'.format(sub), 'ses-movie')
@@ -51,22 +54,33 @@ for sub in sublist:
         subprocess.call('cp {0}/{1} {2}'.format(sub_raw_meg_dir, f, sub_preproc_meg_meg_dir), shell=True)
 
 
-# %% softlink all data to bids_dir
-
+# %% move data to bids_dir
 # raw data
-subprocess.call('ln -s {0} {1}/{2}'.format(raw_data, bids_dir, 'rawdata'), shell=True)
+subprocess.call('mv {0} {1}/{2}'.format(raw_data, bids_dir, 'rawdata'), shell=True)
+
+# freesurfer
+subprocess.call('mv {0} {1}/{2}'.format(freesurfer_data, bids_preproc_dir, 'sourcedata'), shell=True)
 
 # preproced meg and mri
 sublist = ['{0:0>2d}'.format(i) for i in np.arange(1, 12)]
 for sub in sublist:
     bids_preproc_ses_dir = os.path.join(bids_preproc_dir, 'sub-{0}'.format(sub), 'ses-movie')
     if os.path.exists(bids_preproc_ses_dir) is False: os.makedirs(bids_preproc_ses_dir)
-    subprocess.call('ln -s {0}/sub-{1}/ses-movie/anat {2}/anat'.format(preproc_mri, sub, bids_preproc_ses_dir), shell=True)
-    subprocess.call('ln -s {0}/sub-{1}/ses-movie/meg {2}/meg'.format(preproc_meg, sub, bids_preproc_ses_dir), shell=True)
+    subprocess.call('mv {0}/sub-{1}/ses-movie/anat {2}/anat'.format(preproc_mri, sub, bids_preproc_ses_dir), shell=True)
+    subprocess.call('mv {0}/sub-{1}/ses-movie/meg {2}/meg'.format(preproc_meg, sub, bids_preproc_ses_dir), shell=True)
     
     ses_ass_f = glob.glob(os.path.join(preproc_meg, 'sub-{0}'.format(sub), 'ses-movie', ses_ass_files[0]))[0]
-    subprocess.call('ln -s {0} {1}'.format(ses_ass_f, bids_preproc_ses_dir), shell=True)
+    subprocess.call('mv {0} {1}'.format(ses_ass_f, bids_preproc_ses_dir), shell=True)
 
 # copy study associated files
 for f in study_ass_files:
-    subprocess.call('cp {0}/{1} {2}'.format(raw_data, f, bids_dir), shell=True)
+    subprocess.call('cp {0}/{1} {2}'.format(os.path.join(bids_dir, 'rawdata'), f, bids_dir), shell=True)
+
+for f in mri_study_ass_files:
+    subprocess.call('cp {0}/{1} {2}'.format(preproc_mri, f, bids_preproc_dir), shell=True)
+    
+# %%
+tree = mne_bids.print_dir_tree(bids_dir, return_str=True)
+with open('/nfs/s2/userhome/liuxingyu/workingdir/temp/gump_bids', 'w') as f:
+    f.write(tree)
+    
